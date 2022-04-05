cb729cdf7093e137071c86e99084aab7788e3293
Seokho0120@gmail.com <93597794+Seokho0120@users.noreply.github.com>
2022-04-05 15:56:04 +0900

    UI Components 초기세팅

---
+++ src/Router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function Router() {
+  <BrowserRouter>
+    <Routes>
+      <Route />
+    </Routes>
+  </BrowserRouter>;
+}
---
+++ src/login/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Login() {
+  return <Input>아이디 입력하자</Input>;
+}
+
+const Input = styled.div`
+  background-color: red;
+`;
